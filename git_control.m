880872f006611ef67fbb130b0b298bbb6e21078d
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-03-24 21:26:37 -0700

    CommitName

---
+++ git_control.m
@@ -0,0 +1,10 @@
+% cd /home/hnl/Documents/GitHub/1overf
+! git status
+! git add .
+CommitName=char(datetime('now'));
+!git commit -a -m "CommitName"
+% https://docs.github.com/en/get-started/getting-started-with-git/caching-your-github-credentials-in-git
+% https://docs.github.com/en/get-started/getting-started-with-git/caching-your-github-credentials-in-git
+% My zhibin bash script — repo token: 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
+!git push 
+% https://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@github.com/zhibinz2/Motor_cordination.git
