edadc2ee4913c81ff187deb3d67ea67acd0c6f07
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-03-24 22:08:55 -0700

    CommitName

--- git_control.m
+++ git_control.m
@@ -3,8 +3,4 @@
 ! git add .
 CommitName=char(datetime('now'));
 !git commit -a -m "CommitName"
-% https://docs.github.com/en/get-started/getting-started-with-git/caching-your-github-credentials-in-git
-% My zhibin bash script — repo token: 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
-% https://github.blog/2020-12-15-token-authentication-requirements-for-git-operations/
-% https://techglimpse.com/git-push-github-token-based-passwordless/
-!git push https://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/zhibinz2/1overf.git
+
