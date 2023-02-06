edd1016b58e906ae270377d8ed125a2c405b3840
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-02-06 10:24:57 -0800

    update

--- push_to_git.py
+++ push_to_git.py
@@ -4,7 +4,7 @@ import subprocess
 def main():  
     run("add","-A")
     run("commit","-am","update")
-    run("push", "-u", "origin", "master")   
+    # run("push", "-u", "origin", "master")   
 
 def run(*args):
     return subprocess.check_call(['git'] + list(args))
